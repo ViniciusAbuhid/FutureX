b8aa4cbb7674b6794db4b79c8f82afbf9c6097b3
Vinicius <viniciusabuhid@gmail.com>
2020-06-29 17:51:03 -0300

    cloning project

---
+++ build/precache-manifest.5365bc1569a877dd86b44482899e95dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de05ec2fe06c821e9364ffa5a06cdccf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22cc70f5b49cc6a98222",
+    "url": "/static/js/2.419208cd.chunk.js"
+  },
+  {
+    "revision": "d4a0dba354e7677cb5c9",
+    "url": "/static/js/main.44a9336b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "89c08818493112338c9360fea5463f53",
+    "url": "/static/media/Logo.89c08818.PNG"
+  },
+  {
+    "revision": "aab9dd55c0cce103398bb535458cd807",
+    "url": "/static/media/Logo2.aab9dd55.PNG"
+  },
+  {
+    "revision": "7001e9590b764b5447dc5d33c806bb09",
+    "url": "/static/media/alf.7001e959.jpg"
+  },
+  {
+    "revision": "d97e1d58ae34644bef9b147759c076eb",
+    "url": "/static/media/astrodev.d97e1d58.png"
+  }
+]);
